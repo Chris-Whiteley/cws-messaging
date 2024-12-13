5cb061ca647941c2d67eea6af3ba6659c5c17a1d
Chris Whiteley <chris.whiteley81@gmail.com>
2024-12-13 13:46:55 +0000

    added stop() method to Stream interface

--- src/main/java/com/cwsoft/messaging/Stream.java
+++ src/main/java/com/cwsoft/messaging/Stream.java
@@ -4,11 +4,13 @@ import java.util.function.BiConsumer;
 import java.util.function.BiPredicate;
 
 public interface Stream<K, V> {
-    Stream<K, V> filter(BiPredicate<K, V> predicate);
+    Stream<K, V> filter(BiPredicate<K, V> var1);
 
-    void forEach(BiConsumer<K, V> action);
+    void forEach(BiConsumer<K, V> var1);
 
     void start();
+
+    void stop();
 
     void close();
 }
